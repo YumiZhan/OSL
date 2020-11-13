5d556a2267c19e9b08e65d7732fe944b4ffb83b4
zyumi0769 <zyumi0769@outlook.com>
2020-11-14 02:51:44 +0800

    两个子类location和particular，一堆构造函数，应该没什么冲突和遗漏了，反正不能用万恶的函数模板

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,13 +1,94 @@
 ﻿// Matrix.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
 #include "matrix.h"
+#include <typeinfo>
 
 int main()
 {
-    ifstream ifile("..\\matrix\\matrix.txt");
-    matrix m("..\\matrix\\matrix.txt");
-    m.print();
-    return 0;
+	
+	double** a = new double* [3];
+	for (unsigned i = 0; i < 3; i++) {
+		a[i] = new double[3];
+		for (unsigned j = 0; j < 3; j++) {
+			a[i][j] = (i + 1) * (j + 1);
+		}
+	}
+	matrix m1((void**)a, 3U, 3U, NO);
+	for (unsigned i = 0; i < 3; i++) {
+		delete[] a[i];
+	}
+	delete[] a;
+	matrix m2(3.0, 3U, 3U);
+	matrix m3("matrix.txt");
+	matrix m4("matrix.txt", 1, 2);
+	double* p = new double[4];
+	for (unsigned i = 0; i < 4; i++) {
+		p[i] = i + 1;
+	}
+	matrix m5(p, 4);
+	m1.print();
+	m2.print();
+	m3.print();
+	m4.print();
+	m5.print();
+	return 0;
 }
+/*
+double string_to_double(const string& str)
+{
+	char sign = '+';
+	if(str[0])
+	size_t length = str.length();
+	size_t i;
+	double result = 0;
+	for (i = 0; i < length; i++) {
+		if (str[length - 1 - i] == '.') {
+			i++;
+			goto Intiger;
+		}
+		result += str[length - 1 - i] - '0' + 0.1 * result;
+	}
+	return result;
+Intiger:
+	double integer;
+	for(integer = 0;)
+}*/
+/*
+void Add(const matrix& matrix1, const matrix& matrix2, matrix& result)
+{
+	unsigned max_nrows = matrix1.nrows > matrix2.nrows ? matrix1.nrows : matrix2.nrows;
+	unsigned max_ncols = matrix1.ncols > matrix2.ncols ? matrix1.ncols : matrix2.ncols;
+	for (unsigned i = 0; i < N_ROW; i++) {
+		for (unsigned j = 0; j < N_COL; j++) {
+			(result.point)[i][j] = (matrix1.point)[i][j] + (matrix2.point)[i][j];
+		}
+	}
+}
+
+inline matrix operator+(const matrix& matrix1, const matrix& matrix2)
+{
+	matrix result;
+	Add(matrix1, matrix2, result);
+	return result;
+}
+
+void Subtract(const matrix& matrix1, const matrix& matrix2, matrix& result)
+{
+}
+
+inline matrix operator-(const matrix& matrix1, const matrix& matrix2)
+{}
+
+void Multiply(const matrix& matrix1, const matrix& matrix2, matrix& result)
+{
+}
+
+inline matrix operator*(const matrix& matrix1, const matrix& matrix2)
+{}
+
+void Inverse(const matrix& origin, matrix& result)
+{
+}
+
+inline matrix operator/(const matrix& matrix1, const matrix& matrix2)
+{}
+*/
--- Matrix/matrix.h
+++ Matrix/matrix.h
@@ -1,84 +1,105 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-
+#include <string>
+#include <cmath>
 using namespace std;
+
+#define Before -1 //resize position
+#define After 1 //resize position
+#define NO "no" //whether transpose
 
 class matrix {
 public:
+	class location {
+	public:
+		unsigned row, col;
+		location(const location& origin) :row(origin.row), col(origin.col) {}
+		location(unsigned i, unsigned j) :row(i), col(j) {}
+	};
+	class particular {
+	public:
+		location loc;
+		double value, absvalue;
+		particular(const particular& origin) :loc(origin.loc), value(origin.value), absvalue(origin.absvalue) {}
+		particular(location loc, double value) :loc(loc), value(value) { absvalue = fabs(value); }
+		particular(unsigned i, unsigned j, double value) :loc(i, j), value(value) { absvalue = fabs(value); }
+	};
+
 	inline matrix();
-	//matrix(ifstream& ifile, unsigned rows = 0, unsigned cols = 0);
 	matrix(const matrix& origin);
-	matrix(const char* file, unsigned rows = 0, unsigned cols = 0);
-	template <typename Type> matrix(const Type num, const unsigned nsqare = 1);
-	template <typename Type> matrix(const Type* num, const unsigned ncols);
+	matrix(const matrix& origin,
+		unsigned start_row, unsigned start_col,
+		unsigned end_row_next, unsigned end_col_next);
+	typedef const char* path;
+	matrix(path file, unsigned nlines = 0U, unsigned nlists = 0U);
+	matrix(double num, unsigned nsquare = 1U);
+	matrix(double num, unsigned nrows, unsigned ncols);
+	matrix(void* ary, unsigned n, bool transpose = false);
+	matrix(void** ary, unsigned nlines, unsigned nlists, bool transpose = false);
+	
+	//template <typename Type> matrix(const Type[10] * ary, unsigned nlines, unsigned nlists, transpose trsp = false);
 	~matrix();
 
-	inline double* operator[](const unsigned i);
+	inline double* operator[](unsigned i);
+	typedef int position;
+	void append(location loc, position pos = After, unsigned nrows = 0U, unsigned ncols = 0U);
+	void remove(location loc, position pos = After, unsigned nrows = 0U, unsigned ncols = 0U);
 	void print();
-	friend matrix operator+(const matrix& matrix1, const matrix& matrix2);
-	friend matrix operator-(const matrix& matrix1, const matrix& matrix2);
-	friend matrix operator*(const matrix& matrix1, const matrix& matrix2);
-	friend matrix operator/(const matrix& matrix1, const matrix& matrix2);
-	
+
+	friend void Add(
+		const matrix& matrix1, const matrix& matrix2,
+		matrix& result, unsigned start_row, unsigned start_col);
+	friend void Subtract(
+		const matrix& matrix1, const matrix& matrix2,
+		matrix& result, unsigned start_row, unsigned start_col);
+	friend void Multiply(
+		const matrix& matrix1, const matrix& matrix2,
+		matrix& result, unsigned start_row, unsigned start_col);
+	friend void Invert(
+		const matrix& origin,
+		matrix& result, unsigned start_row, unsigned start_col);
+	friend void Overwrite(const matrix& origin, matrix& result);
+
 private:
 	double** point;
 	unsigned nrows, ncols;
+	particular max, min;
+	void max_and_min();
+	void create(
+		const matrix& origin,
+		unsigned start_row, unsigned start_col,
+		unsigned end_row_next, unsigned end_col_next);
 };
 
 inline matrix::matrix() :
-	nrows(1), ncols(1), point(NULL)
+	point(NULL), nrows(1U), ncols(1U), max(0U, 0U, 0.0), min(0U, 0U, 0.0)
 {
 	point = new double*;
 	*point = new double;
 	**point = 0.0;
 }
-/*
-matrix::matrix(ifstream& ifile, unsigned rows, unsigned cols) :
-	nrows(1), ncols(1), point(NULL)
-{
-	if (!ifile) {
-		cout << "can't find file\n";
-		point = new double*;
-		*point = new double;
-		**point = 0.0;
-		return;
-	}
-	if (rows == 0)
-		ifile >> nrows;
-	else
-		nrows = rows;
-	if (cols == 0)
-		ifile >> ncols;
-	else
-		ncols = cols;
-
-	point = new double* [nrows];
-	for (unsigned i = 0; i < nrows; i++) {
-		*point = new double[ncols];
-		for (unsigned j = 0; j < ncols; j++) {
-			ifile >> point[i][j];
-		}
-	}
-	ifile.close();
-}*/
 
 matrix::matrix(const matrix& origin) :
-	nrows(origin.nrows), ncols(origin.ncols), point(NULL)
+	point(NULL), nrows(origin.nrows), ncols(origin.ncols), max(origin.max), min(origin.min)
 {
-	point = new double* [nrows];
-	for (unsigned i = 0; i < nrows; i++) {
-		point[i] = new double[ncols];
-		for (unsigned j = 0; j < ncols; j++) {
-			point[i][j] = origin.point[i][j];
-		}
-	}
+	create(origin, 0U, 0U, nrows, ncols);
 }
 
-matrix::matrix(const char* pfile, unsigned rows, unsigned cols) :
-	nrows(1), ncols(1), point(NULL)
+matrix::matrix(const matrix& origin,
+	unsigned start_row, unsigned start_col,
+	unsigned end_row_next, unsigned end_col_next) :
+	point(NULL), nrows(end_row_next - start_row), ncols(end_col_next - start_col),
+	max(0U, 0U, 0.0), min(0U, 0U, 0.0)
 {
-	ifstream ifile(pfile);
+	create(origin, start_row, start_col, end_row_next, end_col_next);
+	max_and_min();
+}
+
+matrix::matrix(path file, unsigned nlines, unsigned nlists) :
+	nrows(nlines), ncols(nlists), max(0U, 0U, 0.0), min(0U, 0U, 0.0)
+{
+	ifstream ifile(file);
 	if (!ifile) {
 		cout << "can't find file\n";
 		point = new double*;
@@ -86,71 +107,154 @@ matrix::matrix(const char* pfile, unsigned rows, unsigned cols) :
 		**point = 0.0;
 		return;
 	}
-	if (rows == 0)
+	if (!nrows)
 		ifile >> nrows;
-	else
-		nrows = rows;
-	if (cols == 0)
+	if (!ncols)
 		ifile >> ncols;
-	else
-		ncols = cols;
 
 	point = new double* [nrows];
-	for (unsigned i = 0; i < nrows; i++) {
-		*point = new double[ncols];
-		for (unsigned j = 0; j < ncols; j++) {
+	for (unsigned i = 0U; i < nrows; i++) {
+		point[i] = new double[ncols];
+		for (unsigned j = 0U; j < ncols; j++) {
+			//string temp;
 			ifile >> point[i][j];
 		}
 	}
 	ifile.close();
+	max_and_min();
 }
 
-template<typename Type>
-matrix::matrix(const Type num, const unsigned nsqare) :
-	nrows(nsqare), ncols(nsqare), point(NULL)
+//template<typename Type>
+matrix::matrix(double num, unsigned nsquare) :
+	point(NULL), nrows(nsquare), ncols(nsquare), max(0U, 0U, 0.0), min(0U, 0U, 0.0)
 {
 	point = new double* [nrows];
-	for (unsigned i = 0; i < nrows; i++) {
+	for (unsigned i = 0U; i < nrows; i++) {
 		point[i] = new double[ncols];
-		for (unsigned j = 0; j < ncols; j++) {
+		for (unsigned j = 0U; j < ncols; j++) {
 			point[i][j] = 0;
 		}
 		point[i][i] = (double)num;
 	}
+	max_and_min();
 }
 
-template<typename Type>
-matrix::matrix(const Type* num, const unsigned ncols) :
-	nrows(1), ncols(ncols), point(NULL)
+//template<typename Type>
+matrix::matrix(double num, unsigned nlines, unsigned nlists) :
+	max(0U, 0U, 0.0), min(0U, 0U, 0.0)
 {
-	point = new double*;
-	*point = new double[ncols];
-	for (unsigned j = 0; j < ncols; j++) {
-		(*point)[j] = double(num[j]);
+	nrows = nlines, ncols = nlists;
+	point = new double* [nrows];
+	for (unsigned i = 0U; i < nrows; i++) {
+		point[i] = new double[ncols];
+		for (unsigned j = 0U; j < ncols; j++) {
+			point[i][j] = (double)num;
+		}
 	}
+}
+
+//template<typename Type>
+matrix::matrix(void* ary, unsigned n, bool transpose) :
+	nrows(1U), ncols(1U), max(0U, 0U, 0.0), min(0U, 0U, 0.0)
+{
+	unsigned i, j;
+	if (!transpose) {
+		ncols = n;
+		point = new double*;
+		*point = new double[ncols];
+		for (j = 0U; j < ncols; j++) {
+			(*point)[j] = ((double*)ary)[j];
+		}
+	}
+	else {
+		nrows = n;
+		point = new double* [nrows];
+		for (i = 0U; i < n; i++) {
+			point[i] = new double;
+			*(point[i]) = ((double*)ary)[i];
+		}
+	}
+	max_and_min();
+}
+
+//template<typename Type>
+matrix::matrix(void** ary, unsigned nlines, unsigned nlists, bool transpose) :
+	max(0U, 0U, 0.0), min(0U, 0U, 0.0)
+{
+	unsigned i, j;
+	if (!transpose) {
+		nrows = nlines, ncols = nlists;
+		point = new double* [nrows];
+		for (i = 0U; i < nrows; i++) {
+			point[i] = new double[ncols];
+			for (j = 0U; j < ncols; j++) {
+				point[i][j] = ((double**)ary)[i][j];
+			}
+		}
+	}
+	else {
+		nrows = nlists, ncols = nlines;
+		point = new double* [nrows];
+		for (i = 0U; i < nrows; i++) {
+			point[i] = new double[ncols];
+			for (j = 0U; j < ncols; j++) {
+				point[i][j] = ((double**)ary)[j][i];
+			}
+		}
+	}
+	max_and_min();
 }
 
 matrix::~matrix()
 {
-	for (unsigned i = 0; i < nrows; i++) {
+	for (unsigned i = 0U; i < nrows; i++) {
 		delete[] point[i];
 	}
 	delete[] point;
 	point = NULL;
 }
 
-inline double* matrix::operator[](const unsigned i)
+inline double* matrix::operator[](unsigned i)
 {
 	return point[i];
 }
 
 void matrix::print()
 {
-	for (unsigned i = 0; i < nrows; i++) {
-		for (unsigned j = 0; j < ncols; j++) {
+	for (unsigned i = 0U; i < nrows; i++) {
+		for (unsigned j = 0U; j < ncols; j++) {
 			cout << point[i][j];
 			if (j != ncols - 1)cout << '\t';
 		}
 		cout << endl;
+	}
+}
+
+void matrix::create(const matrix& origin,
+	unsigned start_row, unsigned start_col,
+	unsigned end_row_next, unsigned end_col_next)
+{
+	point = new double* [nrows];
+	for (unsigned i = 0U; i < nrows; i++) {
+		point[i] = new double[ncols];
+		for (unsigned j = 0U; j < ncols; j++) {
+			point[i][j] = (origin.point)[i + start_row][j + start_col];
+		}
+	}
+}
+
+void matrix::max_and_min()
+{
+	max = particular(0U, 0U, point[0][0]);
+	min = particular(0U, 0U, point[0][0]);
+	for (unsigned i = 0U; i < nrows; i++) {
+		for (unsigned j = 0U; j < ncols; j++) {
+			if (fabs(point[i][j]) > max.absvalue) {
+				max = particular(i, j, point[i][j]);
+			}
+			if (fabs(point[i][j]) < min.absvalue) {
+				min = particular(i, j, point[i][j]);
+			}
+		}
 	}
 }
